6983d895f13666962eea2d242a8dac5c8d1e88e1
Kevin Rangel Lozada <kevinrangellozada@192.168.1.7>
2023-12-11 00:12:37 -0600

    Validation

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const forge = require('node-forge');
+const e = require("express");
 
+function pemToString(pem) {
+    // Remover los encabezados y pie de página y cualquier salto de línea
+    return pem.replace(/-----BEGIN (.*)-----/, '')
+        .replace(/-----END (.*)-----/, '')
+        .replace(/\r?\n|\r/g, '')
+        .trim();
+}
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,7 +21,7 @@ const transporter = nodemailer.createTransport({
     secure: false, // true for 587, false for other ports
     requireTLS: true,
     auth: {
-        user: 'apolopets666@gmail.com', // Reemplaza con tu correo electrónico
+        user: '', // Reemplaza con tu correo electrónico
         pass: '' // Reemplaza con tu contraseña
     }
 });
@@ -29,6 +38,9 @@ const db = mysql.createConnection({
 });
 
 app.post('/signup', (req, res) => {
+    const keyPair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
+    const publicKeyPem = forge.pki.publicKeyToPem(keyPair.publicKey);
+    const privateKeyPem = forge.pki.privateKeyToPem(keyPair.privateKey);
     const token = crypto.createHash('md5').update(req.body.email + req.body.password).digest('hex');
     const values = [
         req.body.username,
@@ -37,36 +49,51 @@ app.post('/signup', (req, res) => {
         token
     ];
 
-    const sql = 'INSERT INTO users (username, email, password, token, email_confirmed) VALUES (?, false)';
-    db.query(sql, [values], (err, data) => {
+    const sqlkeys = 'insert into userkeys (usertoken, publickey, privatekey) values (?)';
+    db.query(sqlkeys, [[token, publicKeyPem, privateKeyPem]], (err, data) => {
         if(err) {
             console.log(err);
+            const deleteSql = 'DELETE FROM userkeys WHERE usertoken = ?';
+            db.query(deleteSql, [token], (deleteErr, deleteData) => {
+                if (deleteErr) {
+                    console.log(deleteErr);
+                }
+                console.log('Usuario eliminado al generar llaves');
+            });
             return res.status(500).send(`Error al resgistrar usuario`);
         }
 
-        const mailOptions = {
-            from: 'apolopets666@gmail.com',
-            to: req.body.email,
-            subject: 'Confirmación de Registro',
-            text: 'Por favor, confirma tu registro ingresando al siguiente enlace: http://localhost:8081/confirmemail?token=' + token
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                const deleteSql = 'DELETE FROM users WHERE email = ?';
-                db.query(deleteSql, [req.body.email], (deleteErr, deleteData) => {
-                    if (deleteErr) {
-                        console.log(deleteErr);
-                    }
-                    console.log('Usuario eliminado debido al error al enviar el correo');
-                });
-
-                return res.status(500).send(`Error al enviar el correo de confirmación. Usuario no registrado`);
-            } else {
-                console.log('Correo de confirmación enviado: ' + info.response);
-                return res.json("Registro exitoso");
+        const sql = 'INSERT INTO users (username, email, password, token, email_confirmed) VALUES (?, false)';
+        db.query(sql, [values], (err, data) => {
+            if(err) {
+                console.log(err);
+                return res.status(500).send(`Error al resgistrar usuario`);
             }
+
+            const mailOptions = {
+                from: 'apolopets666@gmail.com',
+                to: req.body.email,
+                subject: 'Confirmación de Registro',
+                text: 'Por favor, confirma tu registro ingresando al siguiente enlace: http://localhost:8081/confirmemail?token=' + token
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    const deleteSql = 'DELETE FROM users WHERE email = ?';
+                    db.query(deleteSql, [req.body.email], (deleteErr, deleteData) => {
+                        if (deleteErr) {
+                            console.log(deleteErr);
+                        }
+                        console.log('Usuario eliminado debido al error al enviar el correo');
+                    });
+
+                    return res.status(500).send(`Error al enviar el correo de confirmación. Usuario no registrado`);
+                } else {
+                    console.log('Correo de confirmación enviado: ' + info.response);
+                    return res.json("Registro exitoso");
+                }
+            });
         });
     });
 });
@@ -201,25 +228,40 @@ app.post('/recover3', (req, res) => {
 });
 
 app.post('/buy', (req, res) => {
-    const date = new Date();
+    const sqlgetkeys = 'select * from userkeys where usertoken = ?';
 
-    const billToken = crypto.createHash('md5').update(req.body.token + req.body.description + date).digest('hex');
-    const values = [
-        req.body.username,
-        date,
-        req.body.description,
-        req.body.token,
-        billToken
-    ];
-
-    const sql = 'INSERT INTO bill (username, date, description, usertoken, billtoken) VALUES (?)';
-
-    db.query(sql, [values], (err, data) => {
+    db.query(sqlgetkeys, [req.body.token], (err, data) => {
         if(err){
             console.log(err)
             return res.status(500).send(`Error registrar la compra`);
         }
-        return res.status(200).send("Compra registrada");
+
+        const privateKey = forge.pki.privateKeyFromPem(data[0].privatekey);
+        const date = new Date();
+
+        const billToken = crypto.createHash('md5').update(req.body.token + req.body.description + date.toDateString()).digest('hex');
+        const md = forge.md.md5.create(); // Usamos el algoritmo de hash SHA-256
+        md.update(billToken, 'utf8');
+        const signature = privateKey.sign(md);
+
+        const values = [
+            req.body.username,
+            date.toDateString(),
+            req.body.description,
+            req.body.token,
+            billToken,
+            forge.util.bytesToHex(signature)
+        ];
+
+        const sql = 'INSERT INTO bill (username, date, description, usertoken, billtoken, signature) VALUES (?)';
+
+        db.query(sql, [values], (err, data) => {
+            if(err){
+                console.log(err)
+                return res.status(500).send(`Error registrar la compra`);
+            }
+            return res.status(200).send("Compra registrada");
+        });
     });
 });
 
@@ -242,6 +284,7 @@ app.get('/shoppinglist', (req, res) => {
 });
 
 app.get('/getOneBill', (req, res) => {
+
     const { token } = req.query; // Suponiendo que los datos llegan como parámetros de consulta (query parameters)
 
     // Realiza la consulta a la base de datos para obtener la lista de compras del usuario
@@ -257,9 +300,10 @@ app.get('/getOneBill', (req, res) => {
         else
             return res.status(500).json("Factura no encontrada");
     });
+
 });
 
-app.get('/validatebill', (req, res) => {
+app.get('/validatebill1', (req, res) => {
     const token = req.query.token; // Obtén el token de la solicitud POST
 
     const sql = 'SELECT * FROM bill WHERE billtoken = ?';
@@ -276,7 +320,14 @@ app.get('/validatebill', (req, res) => {
                     <h3>Usuario: ${data[0].username}</h3>
                     <h3>Fecha: ${data[0].date}</h3>
                     <h3>Descripcion: ${data[0].description}</h3>
-                    <h3>Firma: ${data[0].billtoken}</h3>
+                    <h3>Firma: ${data[0].signature}</h3>
+                    <h2>Validar Firma</h2>
+                    <form action="/validatebill2" method="get">
+                        <input type="hidden" name="token" value="${token}">
+                        <label for="password">LLave Publica: </label>
+                        <input type="password" id="publickey" name="publickey" required>
+                        <button type="submit">Validar</button>
+                    </form>
                 </body>
                 </html>
             `);
@@ -285,6 +336,69 @@ app.get('/validatebill', (req, res) => {
             // Si no se encontró ningún token coincidente en la base de datos
             return res.status(500).send(`Firma invalida`);
         }
+    });
+});
+
+app.get('/validatebill2', (req, res) => {
+    const token = req.query.token; // Obtén el token de la solicitud POST
+    try {
+        const publicKey = forge.pki.publicKeyFromPem(req.query.publickey);
+        // Operaciones adicionales con la clave pública
+        const sql = 'SELECT * FROM bill WHERE billtoken = ?';
+        db.query(sql, [token], (err, data) => {
+            if(err){
+                console.log(err);
+                return res.status(500).send(`Error al buscar factura`);
+            }
+            if (data.length > 0) {
+
+                const billToken = crypto.createHash('md5').update(data[0].usertoken + data[0].description + data[0].date).digest('hex');
+                const mdVerify = forge.md.md5.create();
+                mdVerify.update(billToken, 'utf8');
+                let verified = false;
+                try {
+                    verified = publicKey.verify(mdVerify.digest().bytes(), forge.util.hexToBytes(data[0].signature));
+                } catch (error) {
+                    // Captura cualquier error que pueda ocurrir al intentar verificar la firma
+                    console.error('Error al verificar la firma:', error.message);
+                }
+
+                if(verified)
+                    res.send(`
+                        <html>
+                        <body>
+                            <h2>Firma Valida</h2>
+                        </body>
+                        </html>
+                    `);
+                else
+                    return res.status(500).send(`Firma invalida`);
+
+            } else {
+                // Si no se encontró ningún token coincidente en la base de datos
+                return res.status(500).send(`Firma invalida`);
+            }
+        });
+    } catch (error) {
+        return res.status(500).send(`Llave invalida`);
+        // Manejo de errores
+    }
+});
+
+app.get('/getpublickey', (req, res) => {
+    const { token } = req.query;
+
+    const sqlgetkeys = 'select * from userkeys where usertoken = ?';
+
+    db.query(sqlgetkeys, [token], (err, data) => {
+        if(err){
+            console.log(err)
+            return res.status(500).send(`Error al buscar llave`);
+        }
+        if (data.length > 0)
+            return res.status(200).json({publickey: data[0].publickey});
+        else
+            return res.status(500).json("Llave no encontrada");
     });
 });
 
